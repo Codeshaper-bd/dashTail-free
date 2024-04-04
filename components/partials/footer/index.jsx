a6e988a9ef869b3a68d35019ed1d088f9d2996bf
Mahedi Amin <mahedi5544@gmail.com>
2024-04-04 12:37:21 +0600

    update: footer

--- components/partials/footer/index.jsx
+++ components/partials/footer/index.jsx
@@ -1,74 +1,16 @@
 import React from "react";
 import { useSidebar, useThemeStore } from "@/store";
 import { cn } from "@/lib/utils";
-import Link from "next/link";
-import { useMediaQuery } from "@/hooks/use-media-query";
-import MobileFooter from "./mobile-footer";
 import FooterLayout from "./footer-layout";
-import { useMounted } from "@/hooks/use-mounted";
 
-const Footer = ({ handleOpenSearch }) => {
-  const { collapsed, sidebarType } = useSidebar();
+const Footer = ({}) => {
+  const { collapsed } = useSidebar();
   const { layout, footerType } = useThemeStore();
-  const mounted = useMounted();
-  const isMobile = useMediaQuery("(min-width: 768px)");
-
-  if (!mounted) {
-    return null;
-  }
-  if (!isMobile && sidebarType === "module") {
-    return <MobileFooter handleOpenSearch={handleOpenSearch} />;
-  }
-
-  if (footerType === "hidden") {
-    return null;
-  }
-
-  if (layout === "semibox") {
-    return (
-      <div className="xl:mx-20 mx-6">
-        <FooterLayout
-          className={cn(" rounded-md border", {
-            "xl:ml-[72px]": collapsed,
-            "xl:ml-[272px]": !collapsed,
-            "sticky bottom-0": footerType === "sticky",
-          })}
-        >
-          <FooterContent />
-        </FooterLayout>
-      </div>
-    );
-  }
-  if (sidebarType !== "module" && layout !== "horizontal") {
-    return (
-      <FooterLayout
-        className={cn("", {
-          "xl:ml-[248px]": !collapsed,
-          "xl:ml-[72px]": collapsed,
-          "sticky bottom-0": footerType === "sticky",
-        })}
-      >
-        <FooterContent />
-      </FooterLayout>
-    );
-  }
-
-  if (layout === "horizontal") {
-    return (
-      <FooterLayout
-        className={cn("", {
-          "sticky bottom-0": footerType === "sticky",
-        })}
-      >
-        <FooterContent />
-      </FooterLayout>
-    );
-  }
 
   return (
     <FooterLayout
       className={cn("", {
-        "xl:ml-[300px]": !collapsed,
+        "xl:ml-[248px]": !collapsed,
         "xl:ml-[72px]": collapsed,
         "sticky bottom-0": footerType === "sticky",
       })}
