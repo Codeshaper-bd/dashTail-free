c234a29395d65b3749b8abe381d41102bc70bafd
Mahedi Amin <mahedi5544@gmail.com>
2024-04-04 11:46:41 +0600

    Update button.jsx

--- components/ui/button.jsx
+++ components/ui/button.jsx
@@ -13,9 +13,6 @@ const buttonVariants = cva(
         primary: "bg-primary text-primary-foreground hover:bg-primary/80",
         destructive:
           "bg-destructive text-destructive-foreground hover:bg-destructive/80",
-        success: "bg-success text-success-foreground hover:bg-success/80",
-        info: "bg-info text-info-foreground hover:bg-info/80",
-        warning: "bg-warning text-warning-foreground hover:bg-warning/80",
         secondary:
           "bg-secondary text-muted-foreground dark:text-default-950 hover:bg-secondary/80",
         dark: "bg-accent-foreground text-accent hover:bg-accent-foreground/80",
@@ -36,116 +33,6 @@ const buttonVariants = cva(
         icon: "h-10 w-10",
       },
     },
-    compoundVariants: [
-      {
-        variant: "outline",
-        color: "destructive",
-        className:
-          "text-destructive hover:text-destructive-foreground hover:border-destructive hover:bg-destructive",
-      },
-      {
-        variant: "outline",
-        color: "success",
-        className:
-          "text-success hover:text-success-foreground hover:border-success hover:bg-success",
-      },
-      {
-        variant: "outline",
-        color: "info",
-        className:
-          "text-info hover:text-info-foreground hover:border-info hover:bg-info",
-      },
-      {
-        variant: "outline",
-        color: "warning",
-        className:
-          "text-warning hover:text-warning-foreground hover:border-warning hover:bg-warning",
-      },
-      {
-        variant: "outline",
-        color: "dark",
-        className: "text-accent-foreground hover:bg-accent-foreground",
-      },
-      {
-        variant: "outline",
-        color: "secondary",
-        className:
-          "text-muted-foreground dark:bg-transparent hover:bg-default-500  dark:hover:bg-default-500/50 border-default-500",
-      },
-      {
-        variant: "outline",
-        color: "default",
-        className:
-          "text-primary  hover:text-primary-foreground hover:border-primary hover:bg-primary",
-      },
-      {
-        variant: "outline",
-        color: "primary",
-        className: "text-primary  hover:text-primary-foreground",
-      },
-      {
-        variant: "soft",
-        color: "info",
-        className: "text-info hover:text-info-foreground",
-      },
-      {
-        variant: "soft",
-        color: "warning",
-        className: "text-warning hover:text-warning-foreground",
-      },
-      {
-        variant: "soft",
-        color: "destructive",
-        className: "text-destructive  hover:text-destructive-foreground",
-      },
-      {
-        variant: "soft",
-        color: "success",
-        className: "text-success hover:text-success-foreground",
-      },
-      {
-        variant: "soft",
-        color: "secondary",
-        className:
-          "text-muted-foreground dark:bg-opacity-50  hover:bg-default-500/50  dark:hover:bg-opacity-100",
-      },
-      {
-        variant: "soft",
-        color: "default",
-        className: "text-primary",
-      },
-      {
-        variant: "ghost",
-        color: "default",
-        className: " text-primary  ",
-      },
-      {
-        variant: "ghost",
-        color: "secondary",
-        className:
-          " text-muted-foreground dark:bg-transparent hover:bg-default-500/50  dark:hover:bg-default-500/50",
-      },
-      {
-        variant: "ghost",
-        color: "success",
-        className: " text-success  hover:text-success-foreground ",
-      },
-      {
-        variant: "ghost",
-        color: "info",
-        className: " text-info hover:text-info-foreground ",
-      },
-      {
-        variant: "ghost",
-        color: "warning",
-        className: " text-warning hover:text-warning-foreground ",
-      },
-      {
-        variant: "ghost",
-        color: "destructive",
-        className: " text-destructive  hover:text-destructive-foreground ",
-      },
-    ],
 
     defaultVariants: {
       color: "default",
