34e580cba69fbe18adb438ed13a9219cb83c0665
Mahedi Amin <mahedi5544@gmail.com>
2024-04-04 14:29:43 +0600

    Update index.js

--- store/index.js
+++ store/index.js
@@ -4,16 +4,6 @@ import { siteConfig } from "@/config/site";
 export const useSidebar = create((set) => ({
   collapsed: false,
   setCollapsed: (value) => set({ collapsed: value }),
-  sidebarType:
-    siteConfig.layout === "semibox" ? "popover" : siteConfig.sidebarType,
-  setSidebarType: (value) => {
-    set({ sidebarType: value });
-  },
-  subMenu: false,
-  setSubmenu: (value) => set({ subMenu: value }),
-  // background image
-  sidebarBg: siteConfig.sidebarBg,
-  setSidebarBg: (value) => set({ sidebarBg: value }),
   mobileMenu: false,
   setMobileMenu: (value) => set({ mobileMenu: value }),
 }));
