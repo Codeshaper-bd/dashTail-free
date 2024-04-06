ecc91673a0985bff634b6d236ab5c9f801b5f66f
Md. Sakil Anwar <115878208+sakilanwar12@users.noreply.github.com>
2024-04-06 10:55:29 +0600

    add: file uploader

--- app/(dashboard)/(components)/forms/page.jsx
+++ app/(dashboard)/(components)/forms/page.jsx
@@ -5,7 +5,8 @@ import InputElement from "./input-element"
 import CheckboxComponent from "./checkbox-component"
 import RadioComponent from "./radio-component"
 import SelectComponent from "./select-component"
-import { inputCode, checkboxCode, radioCode, selectCode } from "./source-code"
+import { inputCode, checkboxCode, radioCode, selectCode, fileUploader } from "./source-code"
+import FileUploader from "./file-uploader";
 const FormPage = () => {
   return (
     <div className="grid grid-cols-2 gap-6">
@@ -14,15 +15,19 @@ const FormPage = () => {
           <InputElement />
         </div>
       </CardSnippet>
-      <CardSnippet title="Checkbox Component" code={checkboxCode}>
-        <CheckboxComponent />
-      </CardSnippet>
-      <CardSnippet title="Radio Component" code={radioCode}>
-        <RadioComponent />
-      </CardSnippet>
-      <CardSnippet title="Select Component" code={selectCode}>
+      <CardSnippet title="Select" code={selectCode}>
         <SelectComponent />
       </CardSnippet>
+      <CardSnippet title="File Uploader" code={fileUploader}>
+        <FileUploader />
+      </CardSnippet>
+      <CardSnippet title="Checkbox" code={checkboxCode}>
+        <CheckboxComponent />
+      </CardSnippet>
+      <CardSnippet title="Radio" code={radioCode}>
+        <RadioComponent />
+      </CardSnippet>
+
     </div>
   );
 };
--- app/(dashboard)/(components)/forms/source-code.js
+++ app/(dashboard)/(components)/forms/source-code.js
@@ -12,10 +12,6 @@ const InputElement = () => {
         <Label className="mb-3" htmlFor="inputId">Username </Label>
         <Input type="text" placeholder="Enter Your Name" id="inputId" />
       </div>
-      <div className="flex flex-col gap-3">
-        <Label>Upload File</Label>
-        <Input type="file" />
-      </div>
       <div className="flex gap-4">
         <Input type="text" placeholder="with button" id="input" />
         <Button type="submit">Subscribe</Button>
@@ -26,6 +22,53 @@ const InputElement = () => {
 
 export default InputElement;
 `;
+export const fileUploader = `import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { Button } from "@/components/ui/button";
+import { Upload } from "lucide-react";
+const FileUploader = () => {
+  return (
+    <>
+      <div className="flex flex-col gap-3">
+        <Label htmlFor="uploadFile">Upload File</Label>
+        <Input type="file" id="uploadFile" />
+      </div>
+      <div className='flex items-center flex-wrap gap-4 mt-4'>
+        <Label>
+          <Button asChild>
+            <div>
+              <Upload className="mr-2 h-4 w-4" /> Choose File
+            </div>
+          </Button>
+          <Input type="file" className="hidden" />
+        </Label>
+        <Label>
+          <div>
+            <Button asChild color="destructive">
+              <div>
+                Choose File <Upload className="ml-2 h-4 w-4" />
+              </div>
+            </Button>
+          </div>
+          <Input type="file" className="hidden" />
+        </Label>
+
+        <Label>
+          <div>
+            <Button asChild color="info" variant="outline">
+              <div>
+                Choose File <Upload className=" ml-2 h-4 w-4" />
+              </div>
+            </Button>
+          </div>
+          <Input type="file" className="hidden" />
+        </Label>
+      </div>
+    </>
+  );
+};
+
+export default FileUploader;`
 export const checkboxCode = `
 "use client"
 import { Checkbox } from "@/components/ui/checkbox";
