5a9f8851da91713f4ca0b1a69054e51e509fef47
Md. Sakil Anwar <115878208+sakilanwar12@users.noreply.github.com>
2024-04-06 11:32:56 +0600

    add: textarea

---
+++ app/(dashboard)/(components)/forms/text-area.jsx
@@ -0,0 +1,41 @@
+
+import { Label } from "@/components/ui/label";
+import { Textarea } from "@/components/ui/textarea";
+const TextAreaComponents = () => {
+  return (
+    <div className="grid grid-cols-1 lg:grid-cols-2 gap-6">
+      <div>
+        <Label htmlFor="message" className="mb-2">Basic</Label>
+        <Textarea placeholder="What`s on your mind?" rows="3" id="message" />
+      </div>
+      <div>
+        <div className="text-sm font-medium text-default-900 mb-2">Disabled</div>
+        <Textarea placeholder="This textarea is disabled" rows="3" disabled />
+      </div>
+      <div>
+        <div className="text-sm font-medium text-default-900 mb-2">Read Only</div>
+        <Textarea rows="3" readOnly >
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad quos, necessitatibus est quis minima illum. Ullam expedita quo explicabo sapiente.
+        </Textarea>
+      </div>
+      <div>
+        <Label className="mb-2" for="description">Required  Textarea<span className="text-destructive">*</span> </Label>
+        <Textarea placeholder="Description..." id="description" rows="3" required />
+      </div>
+      <div>
+        <div className="text-sm font-medium text-default-900 mb-2">Primary</div>
+        <Textarea placeholder="Type Here.." color="primary" />
+      </div>
+      <div>
+        <div className="text-sm font-medium text-default-900 mb-2">Secondary</div>
+        <Textarea placeholder="Type Here.." color="secondary" />
+      </div>
+      <div>
+        <div className="text-sm font-medium text-default-900 mb-2">Destructive</div>
+        <Textarea placeholder="Type Here.." color="destructive" />
+      </div>
+    </div>
+  );
+};
+
+export default TextAreaComponents;
